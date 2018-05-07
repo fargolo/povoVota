778c93678527fa57267090d8d637f18a6f2afe74
pingfreud <pingfreud@protonmail.com>
2018-05-06 21:29:26 -0300

    moved

--- pvota/static/static_bcp/analysis/simple_ranks.R
+++
@@ -1,4 +0,0 @@
-library(xlsx)
-
-inep2015_df <- read.xlsx("../data/estados-saeb-2015-clean-v2.xlsx",sheetIndex=1)
-head(inep2015_df)
--- pvota/static/static_bcp/setup/inepscrap.R
+++
@@ -1,4 +0,0 @@
-require(RCurl)
-
-download.file("http://download.inep.gov.br/educacao_basica/saeb/aneb_anresc/resultados/resultados_estaduais_saeb_2015.xlsx", destfile = "./estados-saeb-2015.xlsx")
-
